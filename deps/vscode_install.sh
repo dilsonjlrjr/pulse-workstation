bfaed4f607a4a93649e421d9e179601384ddf6d9
dilsonjlrjr <dilsonjlrjr@gmail.com>
2021-07-19 17:00:44 -0300

    Update vscode_install.sh

--- deps/vscode_install.sh
+++ deps/vscode_install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 function vcode_install_dep() {
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > ${COMMON_TEMP_DIR}/microsoft.gpg >> ${COMMON_LOG_FILE} 
-    sudo install -o root -g root -m 644 ${COMMON_TEMP_DIR}/microsoft.gpg /etc/apt/trusted.gpg.d/ >> ${COMMON_LOG_FILE}
-    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    echo "VsCode no deps!" >> ${COMMON_LOG_FILE}
 }
 
 function vcode_install_program() {
-    sudo apt update >> ${COMMON_LOG_FILE}
-    sudo apt install code -y >> ${COMMON_LOG_FILE}
+    sudo snap install code --classic >> ${COMMON_LOG_FILE}
 }
 
 infoMessage "Visual Studio Code" "Preparando a instalação das dependências."
